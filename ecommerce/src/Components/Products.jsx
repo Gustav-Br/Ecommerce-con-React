1f891ec9b16404a887b0a6592dfb492b53d3423b
Gustav-Br <99103909+Gustav-Br@users.noreply.github.com>
2024-08-15 16:53:02 -0300

    preservando la busqueda de productos

--- ecommerce/src/Components/Products.jsx
+++ ecommerce/src/Components/Products.jsx
@@ -11,7 +11,9 @@ function Products() {
     const [producto, setProducto] = useState([]);
     const [louding, setLouding] = useState(true);
     const [input, setInput] = useState('');
-    const [search, setSearch] = useState('celulares');
+    const [search, setSearch] = useState(sessionStorage.getItem('search') || 'celulares');
+    console.log("Input= ", input, "Search= ", search);
+
 
 
     useEffect(() => {
@@ -39,6 +41,7 @@ function Products() {
     const handleSubmit = (event) => {
         event.preventDefault();
         setSearch(input);
+        sessionStorage.setItem('search', input);
     };
     console.log(producto);
     if (louding) {
